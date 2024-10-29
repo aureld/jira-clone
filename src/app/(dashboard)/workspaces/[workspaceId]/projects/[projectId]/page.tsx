d3d0c065b463581c1e8dd9bff0051cabf67dc791
Aurelien Duval <aurelien.duval@gmail.com>
2024-10-30 00:19:43 +0100

    tasks API

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -6,6 +6,7 @@ import { getProject } from "@/features/projects/queries"
 import { ProjectAvatar } from "@/features/projects/components/project-avatar"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import { TaskViewSwitcher } from "@/features/tasks/components/task-view-switcher"
 
 
 interface ProjectIdPageProps {
@@ -46,7 +47,7 @@ const ProjectIdPage = async ({params}: ProjectIdPageProps) => {
           </Button>
         </div>
       </div>
-
+      <TaskViewSwitcher/>
     </div>
   )
 }
